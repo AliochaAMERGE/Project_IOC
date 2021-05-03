48d97727bdf5ff5a51bc226708ede04caf1b9548
aliocha <aliocha.amerge@hotmail.fr>
2021-05-03 13:46:01 +0200

    html & css repaired

--- website/myapp/views.py
+++ website/myapp/views.py
@@ -9,4 +9,3 @@ def index(request):
     template = loader.get_template('index.html')
     data = Data.objects.order_by('-pub_date').all()[0]
     return render(request, 'index.html', {f"value": data.value})
-    # return HttpResponse(f"value: {data.value}")
